9efe2ac662d7f59f56da3f40fa2a5026751e0b56
Jason Ford <fordjason@gmail.com>
2023-06-14 14:06:22 -0700

    Abstract my-content list and add to researcher

--- src/my-content.vue
+++ src/my-content.vue
@@ -1,25 +1,31 @@
 <template>
-  <MyContent expertEditing />
-  <h1>Expert Content</h1>
+  <h1>Content</h1>
+  <button @click="addContent">add content</button>
   <table>
     <thead>
       <tr>
         <th>Type</th>
         <th>Content</th>
-        <th>Admin</th>
+        <th v-if="expertEditing">Expert</th>
         <th></th>
       </tr>
     </thead>
     <tbody>
       <tr
-        v-for="{ assigner }, id in expertContent"
+        v-for="content, id in myContent"
         :key="id"
       >
         <td> {{ id }} </td>
         <td><ContentName :id="id" /></td>
-        <td><UserInfo :user="assigner" name /></td>
+        <td
+          v-if="expertEditing"
+          @click="toggleExpert(id)"
+        >
+          <span v-if="content.expert">TRUE</span>
+          <span v-else>False</span>
+        </td>
         <td>
-          <button @click="toggleExpert(id)">remove</button>
+          <button @click="remove(id)">x</button>
         </td>
       </tr>
     </tbody>
@@ -27,22 +33,20 @@
 </template>
 
 <script>
-  import MyContent from '../../my-content.vue'
-  import ContentName from '../../content-name.vue'
-  import UserInfo from '../../user-info.vue'
+  import ContentName from './content-name.vue'
+  import UserInfo from './user-info.vue'
 
   export default {
     components: {
-      MyContent,
       ContentName,
       UserInfo
+    },
+    props: {
+      expertEditing: Boolean
     },
     computed: {
       myContent() {
         return this.$store.getters['myContent/myContent']()
-      },
-      expertContent() {
-        return this.$store.getters['expertContent/expertContent']()
       }
     },
     methods: {
@@ -50,11 +54,11 @@
         const id = prompt('Enter a content id')
         this.$store.dispatch('myContent/add', id)
       },
-      toggleExpert(id) {
-        this.$store.dispatch('myContent/toggleExpert', id)
-      },
       remove(id) {
         this.$store.dispatch('myContent/remove', id)
+      },
+      toggleExpert(id) {
+        this.$store.dispatch('myContent/toggleExpert', id)
       }
     }
   }
--- src/pages/researcher/index.vue
+++ src/pages/researcher/index.vue
@@ -1,7 +1,12 @@
 <template>
   <h1>Researcher</h1>
   <div v-if="$store.getters['myRole/hasPermission']('researcher')">
-    <TeacherGroups />
+    <div>
+      <button @click="tab = 'teacher-groups'">Teacher Groups</button>
+      <button @click="tab = 'content'">Content</button>
+    </div>
+    <TeacherGroups v-if="tab === 'teacher-groups'"/>
+    <MyContent v-if="tab === 'content'" />
   </div>
   <RoleRequester v-else role="researcher" />
 </template>
@@ -9,11 +14,18 @@
 <script>
   import TeacherGroups from './teacher-groups.vue'
   import RoleRequester from '../../role-requester.vue'
+  import MyContent from '../../my-content.vue'
 
   export default {
     components: {
       TeacherGroups,
+      MyContent,
       RoleRequester
+    },
+    data() {
+      return {
+        tab: 'teacher-groups'
+      }
     }
   }
 </script>
