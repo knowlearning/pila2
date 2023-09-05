3ffe097a9f30be500133d1cfd2c31a1a883d1c5e
Jason Ford <fordjason@gmail.com>
2023-09-05 15:29:41 -0700

    Fix todo for re-tagging content

--- src/store/tags.js
+++ src/store/tags.js
@@ -1,3 +1,20 @@
+function existing_tag(state, content_id, tag_type, user) {
+  let existing = null
+  const entries = Object.entries(state)
+
+  for (let i=0; i<entries.length; i++) {
+    const [id, entry] = entries[i]
+    if ( entry.content_id === content_id
+      && entry.tag_type === tag_type
+      && entry.user_id === user
+    ) {
+      existing = id
+      break
+    }
+  }
+  return existing
+}
+
 export default {
   scope: null,
   namespaced: true,
@@ -24,9 +41,14 @@ export default {
       tags.forEach(tag => commit('add', tag))
     },
     async tag({ commit, dispatch }, { tag_type, content_id, archived=false }) {
-      //  TODO: if tag already exists, return
+      const { auth: { user } } = await Agent.environment()
+      const existing = await existing_tag(state, content_id, tag_type, user)
 
-      Agent.create({
+      if (existing) {
+        const state = await Agent.state(existing)
+        state.archived = false
+      }
+      else Agent.create({
         active_type: 'application/json;type=tag',
         active: { tag_type, content_id, archived }
       })
@@ -34,20 +56,8 @@ export default {
       await dispatch('load')
     },
     async untag({ state, dispatch }, { content_id, tag_type }) {
-      let existing = null
-      const entries = Object.entries(state)
       const { auth: { user } } = await Agent.environment()
-
-      for (let i=0; i<entries.length; i++) {
-        const [id, entry] = entries[i]
-        if ( entry.content_id === content_id
-          && entry.tag_type === tag_type
-          && entry.user_id === user
-        ) {
-          existing = id
-          break
-        }
-      }
+      const existing = await existing_tag(state, content_id, tag_type, user)
 
       if (existing) {
         const state = await Agent.state(existing)
